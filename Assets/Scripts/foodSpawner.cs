13a39e783fc72eb8441e16b0f3a910572579a73d
boxofun1 <kaylab@dhcp-v057-094.mobile.uci.edu>
2016-01-15 22:18:54 -0800

    more food spawner stuff

--- Assets/Scripts/foodSpawner.cs
+++ Assets/Scripts/foodSpawner.cs
@@ -3,11 +3,16 @@ using System.Collections;
 
 public class foodSpawner : MonoBehaviour {
 
+	public GameObject hamburger;
 	private GameObject[] foodPool = new GameObject[20];
 
 	// Use this for initialization
 	void Start () {
-		
+		for (int i = 0; i < 10; ++i) {
+			foodPool[i] = (GameObject)Instantiate (hamburger, new Vector2 (0, 0), Quaternion.identity);
+			foodPool [i].SetActive (false);
+
+		}
 	}
 	
 	// Update is called once per frame
