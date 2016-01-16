36124c2d370777ccd052ea86a89475d3643a9e86
boxofun1 <kaylab@dhcp-v057-094.mobile.uci.edu>
2016-01-15 23:14:22 -0800

    more foooooood

--- Assets/Scripts/foodScript.cs
+++ Assets/Scripts/foodScript.cs
@@ -4,15 +4,24 @@ using System.Collections;
 public class foodScript : MonoBehaviour {
 
 	public int foodVal;
-
+	public foodSpawner foodSpawn;
 
 	// Use this for initialization
 	void Start () {
+		foodSpawn = GameObject.FindGameObjectWithTag("GameController").GetComponent<foodSpawner>();
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void OnCollisionEnter2D(Collision2D hamburgerCollision)
+	{
+		if (hamburgerCollision.gameObject.tag == "Player") {
+			//foodSpawn.counter--;
+			gameObject.SetActive (false);
+		}
 	}
 }
