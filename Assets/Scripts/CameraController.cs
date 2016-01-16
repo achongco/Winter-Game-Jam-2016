de9433b49d3fd3bdcd81faff7cbbee8d0e8a3aba
achongco <achongco@uci.edu>
2016-01-16 12:47:54 -0800

    Begin combining scripts

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = player.transform.position - gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        gameObject.transform.position = player.transform.position - offset;
+          
+	}
+}
