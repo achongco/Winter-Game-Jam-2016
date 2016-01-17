4230119003ef957677e7ad6c12ab86f4ba4220a5
achongco <achongco@uci.edu>
2016-01-16 18:29:01 -0800

    Growing stuff

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,13 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float time;
     public float distance;
+    public float max_Speed = 5f;
     float scale = .7f;
+
     public LayerMask blockingLayer;
     public GameObject belly;
+    public Image hungerBar;
+
+    TimerController tcScript;
 
     private bool move = false;
     private RaycastHit2D hit;
@@ -17,71 +23,46 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rbody = GetComponent<Rigidbody2D>();
+        tcScript = GameObject.FindGameObjectWithTag("GameController").GetComponent<TimerController>();
         Grow();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-
-        int horizontal = 0;
-        int vertical = 0;
-
-        vertical = (int)Input.GetAxisRaw("Vertical");
-        horizontal = (int)Input.GetAxisRaw("Horizontal");
-
-        if (horizontal != 0)
-        {
-            vertical = 0;
-        }
-
-        Debug.Log(vertical);
-        Debug.Log(horizontal);
-
-        if (move == false)
-        {
-            if (horizontal != 0 || vertical != 0)
-            {
-                Vector2 movement = new Vector2(horizontal, vertical);
-                Vector2 start = transform.position;
-                Vector2 end = start + movement * distance;
-                hit = Physics2D.Linecast(start, end, blockingLayer);
-                if (hit.transform == null)
-                    StartCoroutine(MyCoroutine(end));
-
-            }
-        }
-
+        //Directional command handlers
+        float move_x = Input.GetAxis("Horizontal");    //Get input for x-axis
+        float move_y = Input.GetAxis("Vertical");      //Get input for y-axis
+        if (move_x != 0f)
+            move_y = 0f;
+        RotateCharacter(move_x, move_y);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(move_x * max_Speed, move_y * max_Speed);
     }
-
-    IEnumerator MyCoroutine(Vector3 end)
-    {
-        move = true;
-
-        float rate = 1.0f / time;
-
-        float sqrRemainingDistance = (transform.position - end).sqrMagnitude;
-
-
-        while (sqrRemainingDistance > float.Epsilon)
-        {
-            Vector3 newPostion = Vector3.MoveTowards(rbody.position, end, rate * Time.deltaTime);
-            rbody.MovePosition(newPostion);
-            sqrRemainingDistance = (transform.position - end).sqrMagnitude;
-            yield return null;
-            Debug.Log("loop");
-        }
-        
-        move = false;
-    }
-    
     void Grow(){
-        //belly.transform.localScale = new Vector3(1.9f, 1.9f, 1.9f);
-        belly.transform.localScale = new Vector3(2.8f, 2.8f, 2.8f);
-        //belly.transform.localScale = new Vector3(3.7f, 3.7f, 3.7f);
-        //belly.transform.localScale = new Vector3(4.65f, 4.65f, 4.65f);
+        if(tcScript.score>=280000)
+            transform.localScale = new Vector3(3.0f, 3.0f, 3.0f);
+        if (tcScript.score>=210000)
+            transform.localScale = new Vector3(2.7f, 2.7f, 2.7f);
+        else if (tcScript.score>=140000)
+            transform.localScale = new Vector3(1.9f, 1.9f, 1.9f);
+        else if (tcScript.score>=70000)
+            transform.localScale = new Vector3(1.3f, 1.3f, 1.3f);
+    }
+
+    void RotateCharacter(float hori, float vert)
+    {
+        if(vert > 0f) { transform.rotation = Quaternion.Euler(0f, 0f, 0f); }
+        else if(vert < 0f) { transform.rotation = Quaternion.Euler(0f, 0f, 180f); }
+        else if(hori > 0f) { transform.rotation = Quaternion.Euler(0f, 0f, 270f); }
+        else if(hori < 0f) { transform.rotation = Quaternion.Euler(0f, 0f, 90f); }
 
     }
+
+    public void Eat(float fillValue){
+        hungerBar.fillAmount += fillValue;
+        Grow();
+    }
+
 
 
 }
