adfeca647234b94bcb370861a031bc8fe4988eec
sowrd299 <ryan.ward46@gmail.com>
2016-01-17 12:41:15 -0800

    Trapping Prevention
    Avoids getting stuck after growth. (not fully debugged).

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -21,11 +21,21 @@ public class PlayerMovement : MonoBehaviour
     private RaycastHit2D hit;
     Rigidbody2D rbody;
 
+	//colision testing equipment
+	//GameObject tester;
+	CircleCollider2D testColider;
+	Vector3? delayedGrow = null;
+
     // Use this for initialization
     void Start()
     {
 		Eaten = startEaten;
         rbody = GetComponent<Rigidbody2D>();
+		//set up the tester
+		//tester = new GameObject ();
+		//testColider = tester.AddComponent<CircleCollider2D> ();
+		//testColider.radius = GetComponent<CircleCollider2D> ().radius + 0.1f;
+		testColider = GetComponentInChildren<CircleCollider2D>();
     }
 
     void OnTriggerEnter2D(Collider2D col) {
@@ -47,20 +57,42 @@ public class PlayerMovement : MonoBehaviour
                 move_y = 0f;
             RotateCharacter(move_x, move_y);
             GetComponent<Rigidbody2D>().velocity = new Vector2(move_x * max_Speed, move_y * max_Speed);
+			//address cached growths
+			//tester.transform.position = transform.position;
+			if (delayedGrow != null) {
+				GrowTo (delayedGrow);
+			}
         }
         else
             GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 0f);
     }
+
     void Grow(){
         if(Eaten> TIER_SHIFT*4)
-            transform.localScale = new Vector3(3.0f, 3.0f, 3.0f);
-        if (Eaten > TIER_SHIFT*3)
-            transform.localScale = new Vector3(2.7f, 2.7f, 2.7f);
+			GrowTo(new Vector3(3.0f, 3.0f, 3.0f));
+        else if (Eaten > TIER_SHIFT*3)
+			GrowTo(new Vector3(2.7f, 2.7f, 2.7f));
         else if (Eaten >= TIER_SHIFT*2)
-            transform.localScale = new Vector3(1.9f, 1.9f, 1.9f);
+			GrowTo(new Vector3(1.9f, 1.9f, 1.9f));
         else if (Eaten >= TIER_SHIFT)
-            transform.localScale = new Vector3(1.3f, 1.3f, 1.3f);
+			GrowTo(new Vector3(1.3f, 1.3f, 1.3f));
     }
+
+	void GrowTo(Vector3? scale){
+		Debug.Log ("Growing");
+		//makes shure the player will not grow into a collision before they grow 
+		//tester.transform.localScale = (Vector3)scale;
+		if (!testColider.IsTouchingLayers (8)) {
+			GameObject throwAway = new GameObject();
+			throwAway.transform.position = testColider.transform.position;
+			throwAway.AddComponent<CircleCollider2D>().radius = testColider.radius;
+			transform.localScale = (Vector3)scale;
+			delayedGrow = null; //remove the need to grow
+			//debug
+		} else {
+			delayedGrow = scale; //stash the scale to continue trying
+		}
+	}
 
     void RotateCharacter(float hori, float vert)
     {
