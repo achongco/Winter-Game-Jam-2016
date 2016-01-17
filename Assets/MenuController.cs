dca1c5d71c629a25e8cab140040c5c5ca3cc9ed9
sowrd299 <ryan.ward46@gmail.com>
2016-01-16 18:44:53 -0800

    Preliminary Menu, and Made Food Spots Triggers
    see title

---
+++ Assets/MenuController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+	public string level = "scenes/Game1";
+
+
+	public void startGame(){
+		Application.LoadLevel (level);
+	}
+
+}
---
+++ Assets/TextArea.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextArea : MonoBehaviour {
+
+	public GameObject mainButtons;
+	public GameObject credits;
+
+	public void dispMainButtons(){
+		mainButtons.SetActive (true);
+		credits.SetActive (false);
+	}
+
+	public void dispCredits(){
+		mainButtons.SetActive (false);
+		credits.SetActive (true);
+	}
+
+}
